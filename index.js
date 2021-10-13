7cd0efaac2e5a81119df206f32041bfb0e5625c4
Emir Cengiz <emircengiz21@gmail.com>
2021-10-13 20:49:30 +0300

    smooth

---
+++ index.js
@@ -0,0 +1,10 @@
+const google_logo = document.getElementById("main_google_logo");
+
+
+google_logo.onclick = () => {
+    window.scrollBy({
+        top: 800,
+        left: 0,
+        behavior: "smooth"
+    });
+}
